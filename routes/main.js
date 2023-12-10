09e169ef819a294bea900d49e16f678840411b79
APdev93 <lokpedangeran@gmail.com>
2023-12-10 13:33:47 +0800

    fix constanta variabel

--- routes/main.js
+++ routes/main.js
@@ -5,9 +5,27 @@ const router = express.Router();
 
 const { getInfo } = require(`${root}/lib/function.js`);
 
-router.use("/info", (req, res, next) => {
-		let data = getInfo();
-		res.json(data);
+router.use("/info", async (req, res, next) => {
+	let date = new Date();
+	let jam = date.getHours();
+	let menit = date.getMinutes();
+	let detik = date.getSeconds();
+	let old = performance.now();
+	let neww = performance.now();
+	let mili = old / neww;
+	let milsec = mili.toFixed(3);
+	let port = process.env.PORT || 8080 || 5000 || 3000;
+	let status = {
+		status: "online",
+		port: port,
+		time: `${jam} : ${menit} : ${detik} `,
+		speed: `${milsec}ms`,
+		info: {
+			owner: "ardian",
+			apikey: "",
+		},
+	};
+	res.json(status);
 });
 
 module.exports = router;
